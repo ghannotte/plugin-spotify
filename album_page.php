7006885a5532c8f4e96eb71578fb02db430c8922
guillaume.hannotte <guillaume.hannotte@orange.com>
2022-12-19 22:35:01 +0100

    add_album_function

---
+++ album_page.php
@@ -0,0 +1,11 @@
+<?php
+function display_album_page(){ /// cette fonction affiche les informations de lartiste selon les informatons de l'url
+$id= $_GET['id'];//récupération des données de l'url
+$nom= $_GET['nom'];
+$url= $_GET['url'];
+echo $nom .' ' ;
+$image = base64_encode(file_get_contents($url));//récupération récupétation de l'image de l'artiste
+echo '<img width="50" src="data:image/jpeg;base64,'.$image.'">';//affichage de l'image
+}
+
+?>
---
+++ import_album.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'shearch_artist.php';
+function import_album(){;//envoie de la selection utlisateur dans sqlite pour l'album
+$id= $_GET['id'];;//récupération des données de l'url
+$nom= $_GET['nom'];
+$url= $_GET['url'];
+$id_artist= $_GET['id_artist'];
+
+
+$db = new SQLite3('../wp-content/plugins/spotify/spotify_db.db');
+
+$db->exec("INSERT INTO  album VALUES('$id_artist','$id', '$nom','$url')");//envoie des données dans la base artiste de sqlite
+
+$url=$_SERVER['PHP_SELF'].'?page=my-plugin&nom='.$nom.'&id='.$id.'&url='.$url.'&display_album=null' ;
+//echo '<meta http-equiv="Refresh" content="0; url='.$url.'>';//redirection vers la page de l'artiste
+echo '<script type="text/javascript">',
+'window.location.replace("http://localhost'.$url.'");',
+ '</script>'
+;
+shearch_artist_back($id_artist); ///j'en profite pour insérer l'atiste s'il ne l'a pas déja été
+
+}
+
+?>
